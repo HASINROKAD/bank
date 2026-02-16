98f6a7be96a6fc29cca788412688c0f336294470
HASINROKAD <hasinrokad56@gmail.com>
2026-02-16 11:36:12 +0530

    day-1 changes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
 const app = require("./src/app");
+const connectToDB = require("./src/config/db");
+
+connectToDB();
 
 app.listen(3000, () => {
-  console.log("server is running on https://localhost:3000");
+  console.log("server is running on http://localhost:3000");
 });
